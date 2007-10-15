4b09d773989f383841f24f6da8db6fcf5a91b484
Michal Bogusz <sha1-18828411db95aabd95bc2c9596442604e60c7bfa@cern.ch>
2007-10-15 14:30:59 +0000

    new registration utility

--- bin/cmscond_popcon_register.cpp
+++ bin/cmscond_popcon_register.cpp
@@ -25,6 +25,9 @@
 #include <string>
 #include <map>
 
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 using namespace popcon;
 
 class PopConRegister
@@ -45,9 +48,34 @@ class PopConRegister
 			delete m_coraldb;
 			delete session;
 		}
+
+		void commit()
+		{
+			m_coraldb->commit();
+		}
+		void rollback()
+		{
+			m_coraldb->rollback();
+		}
+
+		void doList()
+		{
+			coral::ITable& mytable=m_coraldb->sessionProxy().nominalSchema().tableHandle("P_CON_PAYLOAD_STATE");
+			std::auto_ptr< coral::IQuery > query(mytable.newQuery());
+			query->addToOutputList("NAME");
+			query->addToOutputList("CONNECT_STRING");
+			query->setMemoryCacheSize( 100 );
+			coral::ICursor& cursor = query->execute();
+			while( cursor.next() ){
+				const coral::AttributeList& row = cursor.currentRow();
+				row.toOutputStream( std::cout ) << std::endl;
+			}
+
+		}
+
 		void doRegister(std::string& object, std::string& cstring){
 			try{
-				coral::ITable& mytable=m_coraldb->sessionProxy().nominalSchema().tableHandle("POPCON_LOCK");
+				coral::ITable& mytable=m_coraldb->sessionProxy().nominalSchema().tableHandle("P_CON_LOCK");
 				coral::AttributeList rowBuffer;
 				coral::ITableDataEditor& dataEditor = mytable.dataEditor();
 				dataEditor.rowBuffer( rowBuffer );
@@ -56,14 +84,14 @@ class PopConRegister
 				rowBuffer["LOCK_ID"].data<int>() = 7;
 				dataEditor.insertRow( rowBuffer );
 
-				coral::ITable& mytable2 =m_coraldb->sessionProxy().nominalSchema().tableHandle("POPCON_PAYLOAD_STATE");
+				coral::ITable& mytable2 =m_coraldb->sessionProxy().nominalSchema().tableHandle("P_CON_PAYLOAD_STATE");
 				coral::AttributeList rowBuffer2;
 				coral::ITableDataEditor& dataEditor2 = mytable2.dataEditor();
 				dataEditor2.rowBuffer( rowBuffer2 );
 				rowBuffer2["NAME"].data<std::string>()=object;
+				rowBuffer2["CONNECT_STRING"].data<std::string>()=cstring;
 				dataEditor2.insertRow( rowBuffer2 );
 
-				m_coraldb->commit();
 			}
 			catch(std::exception& er){
 				std::cerr << er.what();
@@ -97,32 +125,93 @@ class PopConRegister
 
 int main(int argc, char** argv)
 {
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " popconConnect payloadName payloadDestinationConnect " << std::endl;
-		exit(EXIT_FAILURE);
+
+	boost::program_options::options_description visible("Usage: cmscond_register_iov [options] \n");
+	visible.add_options()
+		("userConnect,c",boost::program_options::value<std::string>(),"connection string(required)")
+		("popConnect,C",boost::program_options::value<std::string>(),"PopCon connection string(default from cfi file)")
+		("authPath,P",boost::program_options::value<std::string>(),"path to authentication.xml (if not set within shell)")
+		("list,l","list all registered objects(default mode)")
+		("register,r","register the payload")
+		("name,n",boost::program_options::value<std::string>(),"payload class name")
+		("help,h", "help message")
+		;
+	boost::program_options::variables_map vm;
+	try{
+		boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(visible).run(), vm);
+		boost::program_options::notify(vm);
+	}catch(const boost::program_options::error& er) {
+		std::cerr << er.what()<<std::endl;
+		return 1;
+	}
+	if (vm.count("help")) {
+		std::cout << visible <<std::endl;;
+		return 0;
 	}
 
-	std::string connect(argv[1]);
-	std::string myObject(argv[2]);
-	std::string myConnect(argv[3]);
+	bool list=true;
+	std::string userConnect;
+	std::string authPath("");
+	std::string user("");
+	std::string pass("");
+	std::string myObject;
+	std::string popConnect;
+
+
+
+	if(!vm.count("popConnect")){
+		std::cerr <<"[Error] no popConnect[C] option given \n";
+		std::cerr<<" please do "<<argv[0]<<" --help \n";
+		return 1;
+	}else{
+		popConnect=vm["popConnect"].as<std::string>();
+	}
+
+	if( vm.count("authPath") ){
+		authPath=vm["authPath"].as<std::string>();
+		std::string authenv(std::string("CORAL_AUTH_PATH=")+authPath);
+		::putenv(const_cast<char*>(authenv.c_str()));
+	}
+
+	if(vm.count("register")){
+		list = false;
+		if(!vm.count("name")){
+			std::cerr <<"[Error] no name[n] option given \n";
+			std::cerr<<" please do "<<argv[0]<<" --help \n";
+		}
+		else 
+			myObject=vm["name"].as<std::string>();
+		if(!vm.count("userConnect")){
+			std::cerr <<"[Error] no userConnect[c] option given \n";
+			std::cerr<<" please do "<<argv[0]<<" --help \n";
+			return 1;
+		}else{
+			userConnect=vm["userConnect"].as<std::string>();
+		}
+	}
+
+
 
 	PopConRegister * pcr;
 	StateCreator * stc; 
 	try{
-		pcr = new PopConRegister(connect);
-		pcr->doRegister(myObject, myConnect);
-		
-		stc = new StateCreator(connect, myConnect, myObject, false);
-		stc->generateStatusData();
-		stc->storeStatusData();
-		
+		pcr = new PopConRegister(popConnect);
+		stc = new StateCreator(popConnect, userConnect, myObject, false);
+		if (!list){
+			pcr->doRegister(myObject, userConnect);
+			pcr->commit();
+			stc->generateStatusData();
+			stc->storeStatusData();
+			//pcr->commit();
+		}
+		else
+			pcr->doList();
+
 		delete pcr;
 		delete stc;
-
-
 	}
 	catch ( std::exception& e ) {
+		//pcr->rollback();
 		std::cerr << "Error :  " << e.what() << std::endl;
 		exit(EXIT_FAILURE);
 	}
