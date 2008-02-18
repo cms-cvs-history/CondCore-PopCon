82dc6aa13d7498cfd25810a66b22ea2b1dce4be6
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-02-18 13:44:38 +0000

    adding new parameter

---
+++ test/stubs/ExSourceHandler_new.cc
@@ -0,0 +1,75 @@
+#include "ExSourceHandler_new.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+#include<iostream>
+#include<vector>
+
+namespace {
+
+  void fill(Pedestals & p, int nc) {
+    p.m_pedestals.reserve(nc);
+    for(int ichannel=1; ichannel<=nc; ++ichannel){
+      Pedestals::Item item;
+      item.m_mean=1.11*ichannel;
+      item.m_variance=1.12*ichannel;
+      p.m_pedestals.push_back(item);
+    }
+  }
+}
+
+popcon::ExPedestalSource_new::ExPedestalSource_new(const edm::ParameterSet& pset) :
+  m_name(pset.getUntrackedParameter<std::string>("name","ExPedestalSource_new")),
+  p_(pset.getUntrackedParameter<unsigned int >("p")){
+}
+
+popcon::ExPedestalSource_new::~ExPedestalSource_new()
+{
+ 
+}
+
+void popcon::ExPedestalSource_new::getNewObjects() {
+   std::cerr << "------- " << m_name 
+	     << " - > getNewObjects" << std::endl;
+  //check whats already inside of database
+  std::cerr<<"got offlineInfo"<<std::endl;
+ 
+std::cerr << tagInfo().name << ", size " << tagInfo().size 
+            << " last object valid since " 
+	    << tagInfo().lastInterval.first << std::endl;  
+
+
+  if (tagInfo().size>0) {
+    Ref payload = lastPayload();
+    std::cerr<<"size of last payload  "<< 
+      payload->m_pedestals.size()<<std::endl;
+  }
+
+  unsigned int  snc = p_ ;
+   
+   std::cout<<"snc == "<< snc <<std::endl;
+  
+ 
+  std::cerr << "Source implementation test ::getNewObjects : enter first since ? \n";
+ 
+
+
+
+
+
+
+  
+  Pedestals * p0 = new Pedestals;
+  fill(*p0,3);
+  Pedestals * p1 = new Pedestals;
+  fill(*p1,5);
+  Pedestals * p2 = new Pedestals;
+  fill(*p2,7);
+  m_to_transfer.push_back(std::make_pair((Pedestals*)p0,snc));
+  m_to_transfer.push_back(std::make_pair((Pedestals*)p1,snc+20));
+  m_to_transfer.push_back(std::make_pair((Pedestals*)p2,snc+10));
+
+  std::cerr << "------- " << m_name << " - > getNewObjects" << std::endl;
+}
